06f59a1b98643dc53297e53a71c20afd42e34d6f
WanjiruKagema <maureenkagema@gmail.com>
2022-02-10 09:48:07 +0300

    Added H1

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,7 +17,7 @@
     </div>
 </div> --}}
 
-<h1>Posts</h1>
+<h1>Recipes</h1>
 <hr>
 {{-- @if(count($posts)>0)
     @foreach($posts as $post)
@@ -67,47 +67,55 @@
         //     for (var values of CreateRecipe.values()){
         //         console.log(values);
         // }
+      
      const recipeContainer = document.getElementById("posts")
-
 
     recipes.forEach(recipe => {
         recipeContainer.insertAdjacentHTML('beforeend', 
-            `<a href="/recipes/${recipe.id}" id="post">
-                <div class="row border zoom" >
+            `<div class="row border zoom row mt-5">
+            <div class="card-center mt-6" style="height:10%;width:10%;" data-id=${recipe._id}>
+            <div class card-center mt-5> 
+            
+                <span>
+                    <img  style="height: 200px;width:200px; " src="{{url('/storage/cover_images/76094d-splendid-table-noodlewok_1642766175.jpg')}}" />
+                </span> 
+            </div>
+                <a href="/recipes/${recipe._id}" id="post">
+               
                     <span style="padding:20px;">
-                        <h5 class="card-title">${recipe.recipeName}</h5>
+                        <h5 class="card-title">Title <br>${recipe.recipeName}</h5>
                     </span> <br>
 
                     <span style="padding:20px;">
-                        <h5 class="card-title">${recipe.recipeIngredients}</h5>
+                        <h5 class="card-title">Ingredients <br>${recipe.recipeIngredients}</h5>
                     </span> <br>
 
                     <span style="padding:20px;">
-                        <h5 class="card-title">${recipe.recipeMethods}</h5>
+                        <h5 class="card-title">Instructions <br>${recipe.recipeMethods}</h5>
                     </span> <br>
 
                     <p style="margin-right:5px;margin-bottom:0;margin-left:auto;margin-top:auto;"><small>...by Random User</small></p>
-                </div>
-            </a>`
+                
+                </a>
+                <a href="/posts/${recipe._id}" class="button">Edit</a>
+               
+                <input type="button" name="submit" class="button" value="Delete" id="delete" onclick="deleteRecipe('${recipe._id}')" />  
+            </div>
+            </div>`
         );
     });
-    //  document.getElementById("method").innerHTML = recipes[0].recipeMethods;
-    //  document.getElementById("instruction").innerHTML = recipes[0].recipeIngredients;
-     
-
-    // var array =    [[ recipes],
-    //                      ],
-    //             fetch = document.getElementById('fetch');
-    //     for(var i=0; i <array.length; i++){
-    //         var newRow = fetch.insertRow(fetch.length);
-    //         for(var j=0; j<array[i].length; j++){
-    //             var cell = newRow.insertCell(j);
-    //             cell.innerHTML = array[i][j];
-    //         }
-    //     }
            
         })
-       // .catch(err => console.log(err))
+
+        function deleteRecipe(id) {
+               fetch(`http://localhost:3000/recipes/${id}`,{
+                   method: 'DELETE',
+               })
+               .then(res => res.json())
+               .then(() => location.reload())
+            
+        }
+      
 
 
 
