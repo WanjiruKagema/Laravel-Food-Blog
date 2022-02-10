6a5f4b809d4281727f74e36dea24c5a3e6f79637
WanjiruKagema <maureenkagema@gmail.com>
2022-02-10 09:49:01 +0300

    did the PUT request

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -2,36 +2,102 @@
 
 @section('content')
 <br>
-<h1>{{$post->title}}</h1>
-<div class="card-center mt-5" style="height:50%;width:50%;">
-    <img  style="height:100%;width:100%;" src="/storage/cover_images/{{$post->cover_image}}">
+{{-- <h1>{{$post->title}}</h1> --}}
+{{-- <div class="card-center mt-5" style="height:50%;width:50%;">
+    <img  style="height:100%;width:100%;" src="/storage/cover_images/76094d-splendid-table-noodlewok_1642766175.jpg">
 </div>
-<br>
+<br> --}}
 <table>
 <tr>
-<td>{{$post->body}}</td>
-</tr>
-<hr>
-</table>
-<small>Written by {{$post->user->name}} on {{$post->created_at}}</small>
+{{-- <td>{{$post->body}}</td> --}}
+{{-- <a href="/recipes/${recipe.id}" id="put"> </a> --}}
+
+<form action="#"  id="EditRecipe">
+
+    <div class="form-group">
+        Title <input type = "text"  id = "recipeName"   class= "form-control">
+        
+        <br>
+    
+       Ingredients <input type = "text" id="recipeIngredients"  class= "form-control"/>
+       <br>
+       Instructions <input type = "text" id="recipeMethods"   class= "form-control"/>
+
+    </div>
+    <button type="submit" id="edit-post" class="button" onclick="">Submit</button>
+</form>
+   
+{{-- <small>Written by ...by Random User</small> --}}
 <br>
+<div id="posts">
 
-@if(Auth::check())
-    @if(Auth::id()==$post->user->id)
-<a href="/posts/{{$post->id}}/edit" class="button">Edit</a>
+</div>
+@endsection
 
-{!! Form::open(['action'=>['PostsController@destroy',$post->id],'method'=>'POST','class'=>'float-md-right']) !!}
+@section('script')
 
-<form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
+<script>
 
+    const EditRecipe = document.querySelector('#EditRecipe');
+    const recipeName = document.querySelector('#recipeName'); 
+    const recipeIngredients = document.querySelector('#recipeIngredients');
+    const recipeMethods = document.querySelector('#recipeMethods');
+    const btnSubmit = document.querySelector('.button');
 
+     fetch('http://localhost:3000/recipes/{{$recipeId}}', {
+        method: 'GET',
+        // mode: 'no-cors',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        
+    }).then(data => {
+        return data.json();
+    })
+        .then(recipes => {
+            
+            // console.log(recipes);
+            recipeName.value = recipes.recipeName;
+            recipeIngredients.value= recipes.recipeIngredients;
+            recipeMethods.value= recipes.recipeMethods;
 
-{!! Form::hidden('_method', 'DELETE') !!}
+        })
+
     
-    {!! Form::submit('Delete', ['class'=>'button']) !!}
-    
+    EditRecipe.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-{!! Form::close() !!}
-    @endif
-@endif
+        const editvalues = {}
+
+        editvalues.recipeName = recipeName.value,
+        editvalues.recipeIngredients = recipeIngredients.value,
+        editvalues.recipeMethods = recipeMethods.value
+
+        const values = JSON.stringify(editvalues)
+        
+        // send `PUT` request
+        fetch('http://localhost:3000/recipes/{{$recipeId}}', {
+            // send `` request
+            method: 'PUT',
+            // mode: 'no-cors',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            
+            body:values
+            
+        })
+        .then(data => {
+            return data.json();
+        })
+        .then(recipes => {
+            location.replace("/posts")
+            // console.log(recipes)
+    
+        })
+        
+    });
+
+</script>
+    
 @endsection
